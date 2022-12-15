b1f523b3ac0e0322d1d53727c99b3be5c434ad7c
nhachten <nathaniel.hachten@gmail.com>
2022-12-15 09:34:37 -0500

    day 14 part 1

--- Day14/day14.py
+++ Day14/day14.py
@@ -10,6 +10,10 @@ from pprint import pprint
 Position = namedtuple("Position", "right down")
 SAND_DROP = Position(500, 0)
 
+right_low_bound = 999999
+right_high_bound = 0
+down_high_bound = 0 
+
 rock_positions = set()
 sand_positions = set()
 
@@ -27,6 +31,25 @@ def place_rocks(start, end):
             inc_step = -1
         rock_positions.update({Position(r,start.down) for r in range(start.right, end.right + inc_step, inc_step)}) 
 
+    global right_low_bound
+    global right_high_bound
+    global down_high_bound
+
+    if start.right < right_low_bound:
+        right_low_bound = start.right
+    if end.right < right_low_bound:
+        right_low_bound = end.right
+
+    if start.right > right_high_bound:
+        right_high_bound = start.right
+    if end.right > right_high_bound:
+        right_high_bound = end.right
+
+    if start.down > down_high_bound:
+        down_high_bound = start.down
+    if end.down > down_high_bound:
+        down_high_bound = end.down
+
 def blocked(position):
     if position in rock_positions or position in sand_positions:
         return True 
@@ -36,7 +59,6 @@ def blocked(position):
 def drop_sand():
     current_position = SAND_DROP
     still_falling = True
-    inf_fall_check = 0 # TODO check for fall beyond greatest down rock
 
     while still_falling:
         if not blocked(Position(current_position.right, current_position.down + 1)):
@@ -47,20 +69,15 @@ def drop_sand():
             current_position = Position(current_position.right + 1, current_position.down + 1)
         else:
             still_falling = False
-            # TODO make a temp set to add at the end, if bottom out then don't add it, maybe add to a 
-            # new set for infinite path
             sand_positions.add(current_position)
-        inf_fall_check += 1
 
-        # TODO check against the bottom
-        if inf_fall_check > 1000:
+        if current_position.down > down_high_bound:
             print("Detected infinite fall")
             return False
 
     return True
 
 def main(input_lines):
-    # TODO find the bottom
     for line in input_lines:
         rock_lines = line.split(" -> ")
         start = Position(*list(map(int,rock_lines[0].split(','))))
@@ -74,8 +91,22 @@ def main(input_lines):
         sand_stopped = drop_sand()
 
     pprint(sand_positions)
+    print(len(sand_positions))
+
+    for down in range(0, down_high_bound + 1):
+        for right in range(right_low_bound, right_high_bound + 1):
+            if Position(right,down) in sand_positions:
+                print("o",end="")
+            elif Position(right,down) in rock_positions:
+                print("#",end="")
+            else:
+                print(".",end="")
+
+        print()
+
 
 
 if __name__ == "__main__":
-    input_lines = get_input("tiny_input.txt")
+    #input_lines = get_input("tiny_input.txt")
+    input_lines = get_input()
     main(input_lines)
